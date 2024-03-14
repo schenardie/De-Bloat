7b191bff5e884b6c37f46ee00bb1cb80e644ee84
Jose Schenardie <schenardie@gmail.com>
2024-03-14 13:35:01 +1100

    RemediationAdd

---
+++ deplot-detect.ps1
@@ -0,0 +1,4 @@
+if (Test-Path C:\ProgramData\Debloat)
+    {exit 0}
+else 
+    {exit 1}
---
+++ deplot-remediate.ps1
@@ -0,0 +1,25 @@
+$DebloatFolder = "C:\ProgramData\Debloat"
+If (Test-Path $DebloatFolder) {
+    Write-Output "$DebloatFolder exists. Skipping."
+}
+Else {
+    Write-Output "The folder '$DebloatFolder' doesn't exist. This folder will be used for storing logs created after the script runs. Creating now."
+    Start-Sleep 1
+    New-Item -Path "$DebloatFolder" -ItemType Directory
+    Write-Output "The folder $DebloatFolder was successfully created."
+}
+
+$templateFilePath = "C:\ProgramData\Debloat\removebloat.ps1"
+
+Invoke-WebRequest `
+-Uri "https://raw.githubusercontent.com/schenardie/De-Bloat/main/RemoveBloat.ps1" `
+-OutFile $templateFilePath `
+-UseBasicParsing `
+-Headers @{"Cache-Control"="no-cache"}
+
+
+##Populate between the speechmarks any apps you want to whitelist, comma-separated
+$arguments = ' -customwhitelist ""'
+
+
+invoke-expression -Command "$templateFilePath $arguments"
